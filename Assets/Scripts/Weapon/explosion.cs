970e5063e275e2d2a178b86c1da8784a3531e998
Johansson <sebbe.johansson1995@gmail.com>
2016-09-02 19:06:50 +0200

    Explosioner och annat
    Simons skit.

---
+++ Assets/Scripts/Weapon/explosion.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerStay(Collider coll)
+    {
+        lineOfSight(coll.gameObject);
+    }
+
+    // Checks if target is in line of sight
+    bool lineOfSight(GameObject target)
+    {
+        print(target);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, (target.transform.position - transform.position), out hit))
+        {
+            if(hit.collider == target)
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.green);
+                Debug.Log("BOom");
+                return true;
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+                Debug.Log("no");
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
