dd7ac0eff8e35c2bf0aa093b08530e72f83c8d1c
Sebasitan Johansson <sebbe.johansson1995@gmail.com>
2016-09-14 18:15:29 +0200

    Powerups
    Added initial support for powerups.
    
    Added healthkit and armorkit.

---
+++ Assets/Scripts/Powerups/armorkit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class armorpack : powerupBase {
+
+	public override void activatePowerup (GameObject player)
+	{
+		Debug.LogWarning ("Picked up an Armor kit");
+		plyStats.addArmor (10);
+	}
+
+}
+
